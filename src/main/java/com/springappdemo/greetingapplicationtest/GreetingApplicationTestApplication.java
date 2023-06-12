f90c90a8512eb54a3260d37369ed458532566d66
Tanujachaugule <tanujachaugule1999@gmail.com>
2023-06-12 22:17:52 +0530

    Added: Spring Greeting App

---
+++ src/main/java/com/springappdemo/greetingapplicationtest/GreetingApplicationTestApplication.java
@@ -0,0 +1,13 @@
+package com.springappdemo.greetingapplicationtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreetingApplicationTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreetingApplicationTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springappdemo/greetingapplicationtest/GreetingApplicationTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springappdemo.greetingapplicationtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreetingApplicationTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
