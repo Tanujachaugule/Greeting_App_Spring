bc6fda0556b871a656e7f14dc22454bd09fe9a55
Tanujachaugule <tanujachaugule1999@gmail.com>
2023-06-13 13:36:55 +0530

    Added: Ability to save message in the reopsitory

--- src/main/java/com/springappdemo/greetingapplicationtest/configuration/WebConfiguration.java
+++ src/main/java/com/springappdemo/greetingapplicationtest/configuration/WebConfiguration.java
@@ -1,1 +1,15 @@
+package com.springappdemo.greetingapplicationtest.configuration;
 
+import org.springframework.context.annotation.Configuration;
+
+
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+@Configuration
+
+public class WebConfiguration {
+    ServletRegistrationBean h2servletRegistration(){
+        ServletRegistrationBean registrationBean = new ServletRegistrationBean();
+        registrationBean.addUrlMappings("/h2/*");
+        return registrationBean;
+    }
+}
